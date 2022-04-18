b0c5e7cdee49a8643e88c93a7d9780458a46b977
Eri Uchino <eri@ERIs-MacBook-Air.local>
2022-04-18 15:38:19 +0900

    create existence_check for email and password

---
+++ src/test/resources/jp/co/seattle/library/service/UsersServiceTest.java
@@ -0,0 +1,14 @@
+package jp.co.seattle.library.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UsersServiceTest {
+
+	@Test
+	public void test() {
+		fail("まだ実装されていません");
+	}
+
+}
